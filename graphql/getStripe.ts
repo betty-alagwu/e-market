e59a049222ecc03c1d87c8737769cfa2f38062e4
Betty Alagwu <bettyalagwu@gmail.com>
2023-01-25 22:44:23 +0100

    new commit

--- graphql/getStripe.ts
+++ graphql/getStripe.ts
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect} from "react";
+
 import { loadStripe, Stripe } from "@stripe/stripe-js";
 
 // const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
